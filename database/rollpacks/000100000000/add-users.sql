dae52d44ce28ba762abda7afa0a11452d60e57eb
Yuriy Lazutin <yuriylazutin@gmail.com>
2021-04-12 22:41:40 +0700

    Added rollpack 000100000000 (initial database installation)

---
+++ database/rollpacks/000100000000/add-users.sql
@@ -0,0 +1,3 @@
+create user "BASE" identified by "&1" default tablespace "BASE" temporary tablespace "TEMP";
+create user "COMMON" identified by "&1" default tablespace "COMMON" temporary tablespace "TEMP";
+create user "DEV" identified by "&1" default tablespace "DEV" temporary tablespace "TEMP";
